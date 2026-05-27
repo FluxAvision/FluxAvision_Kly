3dd49eec4a9e5f02005c766ccae5a21196ba7395
Alex <george.chongzhi@gmail.com>
2026-05-28 00:18:29 +0800

    cleanup: 移除临时调试脚本

--- _temp_find.py
+++
@@ -1,5 +0,0 @@
-with open(r'D:\FluxAvision\FluxAvision_Kly\frontend-vue\src\components\large-screen\designer\PropertyPanel.vue', encoding='utf-8') as f:
-    lines = f.readlines()
-for i, line in enumerate(lines):
-    if 'type="color"' in line:
-        print(f'Line {i+1}: {repr(line)}')
